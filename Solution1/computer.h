937cff4cf8b8a779271d1b72dae48518d9d68a1d
Elin <elinh14@ru.is>
2014-12-07 19:16:50 +0000

    prufaennogaftur

--- Solution1/computer.h
+++ Solution1/computer.h
@@ -1,10 +1,19 @@
 #ifndef COMPUTER_H
 #define COMPUTER_H
 
+#include <ctime>
+#include <string>
+
+
 class Computer
 {
 public:
     Computer();
+    bool operator==(const Scientist &rhs);
+    std::string brand;
+    std::string year;
+    std::string type;
+    std::string built;
 };
 
 #endif // COMPUTER_H
